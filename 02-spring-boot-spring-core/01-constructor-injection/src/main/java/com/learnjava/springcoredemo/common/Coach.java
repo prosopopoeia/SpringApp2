16f457f9cc8535e3d9c6b80f5d562690c36f14fb
Solomon Grundy <ngwtt@yahoo.com>
2024-08-11 06:37:07 -0600

    housekeeping

--- 02-spring-boot-spring-core/01-constructor-injection/src/main/java/com/learnjava/springcoredemo/common/Coach.java
+++ 02-spring-boot-spring-core/01-constructor-injection/src/main/java/com/learnjava/springcoredemo/common/Coach.java
@@ -1,4 +1,4 @@
-package com.learnjava.springcoredemo;
+package com.learnjava.util;
 
 public interface Coach {
 
--- 02-spring-boot-spring-core/01-constructor-injection/src/main/java/com/learnjava/springcoredemo/common/CricketCoach.java
+++ 02-spring-boot-spring-core/01-constructor-injection/src/main/java/com/learnjava/springcoredemo/common/CricketCoach.java
@@ -1,4 +1,4 @@
-package com.learnjava.springcoredemo;
+package com.learnjava.util;
 
 import org.springframework.stereotype.Component;
 
